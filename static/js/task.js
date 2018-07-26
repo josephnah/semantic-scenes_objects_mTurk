b0360df47dd6c9924bd980d0b72797d7c828476c
Joseph Nah_lab <joecool890@gmail.com>
2018-07-26 15:38:26 -0400

    # finalized timing, need to work on the following:  - Response Recording (started but need to finish)  - Collecting RT  - Saving data  - work out small kinks in gabor patch

--- static/js/task.js
+++ static/js/task.js
@@ -16,13 +16,14 @@ var psiTurk = new PsiTurk(uniqueId, adServerLoc, mode);
 ********************/
 var trial_matrix = [];
 var trial_matrix_file = "/static/trial-type.csv";
-var prac_length = 4;
+var prac_length = 10;
 var pages = [
 	"instructions/instruct-ready.html",
 	"stage.html",
 	"postquestionnaire.html"
 ];
 // sizes of images utilized in experiment
+var screen_size         = [1080, 800];
 var scene_size          = [640, 512];
 var object_size         = [150, 150];
 var target_gabor_size   = 50;
@@ -30,7 +31,7 @@ var center_gabor_size   = 20;
 var object_coordinate   = 200;
 
 // x, y Coordinates for stimuli positioning
-var center              = [1080/2, 800/2];
+var center              = [screen_size[0]/2, screen_size[1]/2];
 var left_loc_object     = [center[0] - object_size[0]/2 - object_coordinate, center[1] - object_size[1]/2];
 var right_loc_object    = [center[0] - object_size[0]/2 + object_coordinate, center[1] - object_size[1]/2];
 var left_loc_gabor      = [center[0] - target_gabor_size/2 - object_coordinate, center[1] - target_gabor_size/2];
@@ -57,10 +58,18 @@ var scene_path = [
 var img_file_ext    = ".png";
 
 // Labels to make reading files easy
-var index_scene_category  = 0;
-var index_main_object     = 1;
-var index_other_object    = 2;
-var index_main_object_loc = 3;
+var index_scene_category    = 0;
+var index_main_object       = 1;
+var index_other_object      = 2;
+var index_main_object_loc   = 3;
+var index_target_loc        = 4;
+
+// Presentation time
+var ITI                     = 500;
+var scene_display_time      = 1000;
+var object_display_time     = 750;
+var gabor_display_time      = 200;
+var get_response_time       = 2000;
 
 // General function that loads images.
 // var Image = function Show_image(image_path, width, height, cx, cy) {
@@ -93,9 +102,9 @@ function readTextFile(file, shuffle){
         }
     };
     rawFile.send(null);
-};
+}
 readTextFile(trial_matrix_file,1);
-console.log(trial_matrix);
+// console.log(trial_matrix);
 
 /********************
 * HTML manipulation
@@ -112,16 +121,11 @@ console.log(trial_matrix);
 *******************/
 
 var practice = function() {
-	// pop off first row in trial matrix
     document.body.style.cursor = 'none';
-    console.log(trial_matrix);
+
     // count trial # of practice
     var prac_trial_count = 0;
-    console.log(prac_trial_count);
-    /* information for practice trials
-       [x][y]
-       [x] = row
-       [y] = column     */
+    // information for practice trials [x = row][y = column]
     prac_trials = trial_matrix;
 
     // function that dictates stimuli presentation order/duration
@@ -133,57 +137,40 @@ var practice = function() {
 			clearTimeout();
 			finish();
 		} else {
-            show_fixation_original();
-			clearTimeout();
+            show_fixation();
+			// clearTimeout(); NOT sure if this is needed
 
-			handle01 = setTimeout(function(){
-				show_fixation_red();}, 500); // Present this 500 ms after previous
-			handle02 = setTimeout(function(){
-				show_scene();}, 1000);
-			handle03 = setTimeout(function(){
-				show_objects();}, 2000);
-			handle04 = setTimeout(function(){
-			    show_gabors();},2100);
+            // setTimeout function activates AFTER inputted time
+			disp_scene = setTimeout(function(){
+				show_scene();}, ITI);
+			disp_objects = setTimeout(function(){
+				show_objects();}, ITI + scene_display_time);
+			disp_targets = setTimeout(function(){
+			    show_gabors();}, ITI + scene_display_time + object_display_time);
             handle05 = setTimeout(function(){
-			    show_SOA();},3000);
+			    show_SOA();}, ITI + scene_display_time + object_display_time + gabor_display_time);
 		}
 	};
 
     var finish = function() {
 		var s = Snap('#svgMain');
-		var between_prac = s.image("/static/images/AfterPrac.png", 0,200);
+		var after_prac = s.image("/static/images/AfterPrac.png", 0,200);
 	};
 
     //* * Stimuli used in experiment, will repeat for practice and experiment for now
-    var show_fixation_original = function(){
-        this.s = Snap("#svgMain"); // initiate scalable vector graphics (think of canvas to draw on)
-        this.vertical = s.line(center[0]-10, center[1], center[0]+10, center[1]);
-        this.vertical.attr({
-          id:"fix1",
-          stroke: "#ffffff",
-          strokeWidth: fixation_width
-        });
-        this.horizontal = this.s.line(center[0], center[1]-10, center[0], center[1]+10);
-        this.horizontal.attr({
-          id:"fix2",
-          stroke: "#ffffff",
-          strokeWidth: fixation_width
-        });
 
-    };
-
-    var show_fixation_red = function(){
+    var show_fixation = function(){
         this.s = Snap("#svgMain"); // initiate scalable vector graphics (think of canvas to draw on)
         this.vertical = this.s.line(center[0]-10, center[1], center[0]+10, center[1]);
         this.vertical.attr({
           id:"fix1",
-          stroke: "#da2822",
+          stroke: "#ffffff",
           strokeWidth: fixation_width
         });
         this.horizontal = this.s.line(center[0], center[1]-10, center[0], center[1]+10);
         this.horizontal.attr({
           id:"fix2",
-          stroke: "#da2822",
+          stroke: "#ffffff",
           strokeWidth: fixation_width
         });
     };
@@ -196,13 +183,13 @@ var practice = function() {
         this.vertical = this.s.line(center[0]-10, center[1], center[0]+10, center[1]);
         this.vertical.attr({
           id:"fix1",
-          stroke: "#da2822",
+          stroke: "#ffffff",
           strokeWidth: fixation_width
         });
         this.horizontal = this.s.line(center[0], center[1]-10, center[0], center[1]+10);
         this.horizontal.attr({
           id:"fix2",
-          stroke: "#da2822",
+          stroke: "#ffffff",
           strokeWidth: fixation_width
         });
     };
@@ -215,7 +202,7 @@ var practice = function() {
         this.main_object_path   = "static/images/objects/" + this.main_object_stim + img_file_ext; // path to image
         this.other_object_path  = "static/images/objects/" + this.other_object_stim + img_file_ext; // path to image
 
-        if ([prac_trials[prac_trial_count][index_main_object_loc]] === [1]) {
+        if (parseInt([prac_trials[prac_trial_count][index_main_object_loc]]) === 1) {
             this.main_object_location_x     = left_loc_object[0];
             this.main_object_location_y     = left_loc_object[1];
             this.other_object_location_x    = right_loc_object[0];
@@ -225,7 +212,7 @@ var practice = function() {
             this.main_object_location_y     = right_loc_object[1];
             this.other_object_location_x    = left_loc_object[0];
             this.other_object_location_y    = left_loc_object[1];
-        };
+        }
 
         this.main_object    = this.s.image(this.main_object_path, this.main_object_location_x, this.main_object_location_y, object_size[0], object_size[1]);
         this.other_object   = this.s.image(this.other_object_path, this.other_object_location_x, this.other_object_location_y, object_size[0], object_size[1]);
@@ -251,36 +238,82 @@ var practice = function() {
             var match = 0;
             var center_ori = -target_ori;
         };
-        this.target_gabor = this.s.image("static/images/gabors/gabor02.png", left_loc_gabor[0], left_loc_gabor[1], 50, 50);
-        this.center_gabor = this.s.image("static/images/gabors/gabor01.png", center_loc_gabor[0], center_loc_gabor[1], 20, 20);
+
+        if (parseInt([prac_trials[prac_trial_count][index_target_loc]]) === 1) {
+            this.gabor_location = left_loc_gabor
+        } else {
+            this.gabor_location = right_loc_gabor
+        }
+        this.target_gabor = this.s.image("static/images/gabors/gabor02.png", this.gabor_location[0], this.gabor_location[1], 50, 50);
+        this.center_gabor = this.s.image("static/images/gabors/gabor01.png", center_loc_gabor[0],center_loc_gabor[1], 20, 20);
         // rotates gabor patch
-        this.target_gabor.transform("t"+parseInt(target_ori));
-        this.center_gabor.transform("t"+parseInt(center_ori));
+        this.target_gabor.transform("r"+parseInt(target_ori));
+        this.center_gabor.transform("r"+parseInt(center_ori));
 
     };
 
+    // var get_response = function (e) {
+    //     if (e.charCode === 102 || e.charCode === 106) { // 102f 106j
+    //         clearTimeout();
+    //         resp = 1;
+    //         if (e.charCode === 102 & match === 1) {
+    //             acc = 1;
+    //         } else if (e.charCode === 106 & match === 2) {
+    //             acc = 1;
+    //         }
+    //         } else {
+    //
+    //     }
+    //
+    //     } else {
+    //         resp = 0;
+    //     }}
+    // };
+
     var show_SOA = function(){
-		var s = Snap('#svgMain');
-		s.clear();
-		var horizontal = s.line(530,400,550,400);
-		horizontal.attr({
-		  id:"fix1",
-		  stroke: "white",
-		  strokeWidth:5
-		});
-		var vertical = s.line(540, 390, 540, 410);
-		vertical.attr({
-		  id:"fix2",
-		  stroke: "white",
-		  strokeWidth:5
-		});
+        this.s = Snap("#svgMain");
+        this.scene_stim = scene_path[prac_trials[prac_trial_count][index_scene_category]-1];
+        this.scene = this.s.image(this.scene_stim, center[0] - scene_size[0]/2, center[1] - scene_size[1]/2, scene_size[0], scene_size[1]);
+        this.main_object_stim   = [prac_trials[prac_trial_count][index_main_object]];
+        this.other_object_stim  = [prac_trials[prac_trial_count][index_other_object]];
+        this.main_object_path   = "static/images/objects/" + this.main_object_stim + img_file_ext; // path to image
+        this.other_object_path  = "static/images/objects/" + this.other_object_stim + img_file_ext; // path to image
+
+        if (parseInt([prac_trials[prac_trial_count][index_main_object_loc]]) === 1) {
+            this.main_object_location_x     = left_loc_object[0];
+            this.main_object_location_y     = left_loc_object[1];
+            this.other_object_location_x    = right_loc_object[0];
+            this.other_object_location_y    = right_loc_object[1];
+        } else {
+            this.main_object_location_x     = right_loc_object[0];
+            this.main_object_location_y     = right_loc_object[1];
+            this.other_object_location_x    = left_loc_object[0];
+            this.other_object_location_y    = left_loc_object[1];
+        }
+
+        this.main_object    = this.s.image(this.main_object_path, this.main_object_location_x, this.main_object_location_y, object_size[0], object_size[1]);
+        this.other_object   = this.s.image(this.other_object_path, this.other_object_location_x, this.other_object_location_y, object_size[0], object_size[1]);
+
+        this.vertical = this.s.line(center[0]-10, center[1], center[0]+10, center[1]);
+        this.vertical.attr({
+          id:"fix1",
+          stroke: "#ffffff",
+          strokeWidth: fixation_width
+        });
+        this.horizontal = this.s.line(center[0], center[1]-10, center[0], center[1]+10);
+        this.horizontal.attr({
+          id:"fix2",
+          stroke: "#ffffff",
+          strokeWidth: fixation_width
+        });
+
         prac_trial_count ++;
         // console.log(prac_trial_count)
 		clearTimeout(handle05);
 		// clearTimeout();
 
 		handle_fin = setTimeout(function(){
-		    next()},500);
+		    next()}, get_response_time);
 	};
 
     // Load the stage.html snippet into the body of the page
