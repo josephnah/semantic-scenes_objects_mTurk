f665e1a6f7f593f4c687a4f05072a3fbf465bce9
Joseph Nah <joecool890@gmail.com>
2018-08-29 01:10:27 -0400

    # Now has practice + Experiment phase
    Signed-off-by: Joseph Nah <joecool890@gmail.com>

--- static/js/task.js
+++ static/js/task.js
@@ -14,8 +14,9 @@ var psiTurk = new PsiTurk(uniqueId, adServerLoc, mode);
 * to be used in the experiment
 *
 ********************/
-var trial_matrix = [];
-var trial_matrix_file = "/static/trial-type.csv";
+var trial_matrix        = [];
+var prac_trial_matrix   = [];
+var trial_matrix_file   = "/static/trial-type.csv";
 var pages = [
 	"instructions/instruct-1.html",
     "instructions/instruct-2.html",
@@ -77,7 +78,7 @@ var gabor_display_time      = 200;
 var get_response_time       = 2000;
 
 // Misc. Variables
-var practice_length = 20;
+var prac_length     = 20;
 var block_size      = 80;
 var total_blocks    = 4;
 
@@ -114,8 +115,9 @@ function readTextFile(file, shuffle){
                 trial_matrix.push(tabs);
             });
             if (shuffle === 1){
-            trial_matrix1 = _.shuffle(trial_matrix);
-            trial_matrix2 = _.shuffle(trial_matrix);
+            trial_matrix1       = _.shuffle(trial_matrix);
+            trial_matrix2       = _.shuffle(trial_matrix);
+            prac_trial_matrix   = _.shuffle(trial_matrix);
             trial_matrix = trial_matrix1.concat(trial_matrix2);
             }
         }
@@ -126,47 +128,34 @@ readTextFile(trial_matrix_file,1);
 
 var object_location = [];
 
-
-// console.log(arr);
-/********************
-* HTML manipulation
-*
-* All HTML files in the templates directory are requested 
-* from the server when the PsiTurk object is created above. We
-* need code to get those pages from the PsiTurk object and 
-* insert them into the document.
-*
-********************/
-
 /*******************
 *  Practice Block  *
 *******************/
 
 var practice = function() {
     document.body.style.cursor = 'none';
-    var exp_length = trial_matrix.length-1;
-    console.log(prac_length);
+
     // count trial # of practice
     var prac_trial_count = 0;
-    console.log("start of trial:" + prac_trial_count);
+    // console.log("start of trial:" + prac_trial_count);
 
     // Array for object location randomization
     for (i = 0; i <= prac_length; i++) {
         object_location[i] = Math.random();
     }
 
-    // randomization for gabor orientations & orientation match
+    // initialize gabor orientations & orientation match variables
     var match;
     var target_ori;
     // information for practice trials [x = row][y = column]
-    prac_trials = trial_matrix;
+    prac_trials = prac_trial_matrix;
 
     // function that dictates stimuli presentation order/duration
     var next = function() {
-        var s = Snap("#svgMain");
+        this.s = Snap("#svgMain");
         s.clear();
         // finish practice
-        if (prac_trial_count ===prac_length) {
+        if (prac_trial_count === prac_length) {
 			clearTimeout();
 			finish();
 		} else {
@@ -187,17 +176,17 @@ var practice = function() {
 	};
 
     var finish = function() {
-		var s = Snap('#svgMain');
-		this.s.image("/static/images/AfterPrac.png", 0,200);
-		document.addEventListener("keydown",to_questionnaire, false);
+		this.s = Snap('#svgMain');
+		this.s.image("/static/images/after_prac.png", 0,200);
+		document.addEventListener("keydown",finishprac, false);
 	};
 
-    var to_questionnaire = function(e){
-        if (e.keyCode == 32) {
-            document.removeEventListener("keydown",to_questionnaire,false);
-            currentview = new Questionnaire();
-        }
-    };
+	var finishprac = function(e){
+			if (e.keyCode === 32) {
+				document.removeEventListener("keydown",finishprac,false);
+				currentview = new main_task();
+			}
+	};
 
     var show_objects = function() {
         this.s = Snap("#svgMain");
@@ -317,13 +306,212 @@ var practice = function() {
                 disp_plz_respond()}, 100);
 
 
+        };
+    };
+
+    var disp_plz_respond = function() {
+        this.s = Snap("#svgMain");
+        s.clear();
+        show_fixation();
+
+        handle_fin = setTimeout(function(){
+		    next()}, 1000);
+    };
+
+    // Load the stage.html snippet into the body of the page
+	psiTurk.showPage('stage.html');
+
+	next();
+
+};
+
+
+/*****************
+ *   Main Task   *
+ *****************/
+
+var main_task = function() {
+    document.body.style.cursor = 'none';
+    var exp_length = trial_matrix.length-1;
+    console.log(exp_length);
+    // count trial # of practice
+    var trial_count = 0;
+    console.log("start of trial:" + trial_count);
+
+    // Array for object location randomization
+    for (i = 0; i <= exp_length; i++) {
+        object_location[i] = Math.random();
+    }
+
+    // randomization for gabor orientations & orientation match
+    var match;
+    var target_ori;
+    // information for practice trials [x = row][y = column]
+    exp_trials = trial_matrix;
+
+    // function that dictates stimuli presentation order/duration
+    var next = function() {
+        var s = Snap("#svgMain");
+        s.clear();
+        // finish practice
+        if (trial_count === exp_length) {
+			clearTimeout();
+			finish();
+		} else {
+            document.removeEventListener("keypress", get_response, false); // Just in case
+            show_fixation();
+			clearTimeout(); //NOT sure if this is needed
+
+            // setTimeout function activates AFTER inputted time
+			disp_scene = setTimeout(function(){
+				show_scene("static/images/scenes/" + [exp_trials[trial_count][index_scene_category]] +"/" +[exp_trials[trial_count][index_scene_exemplar]]+ img_file_ext);}, ITI_time);
+			disp_objects = setTimeout(function(){
+				show_objects();}, ITI_time + scene_display_time);
+			disp_targets = setTimeout(function(){
+			    show_gabors();}, ITI_time + scene_display_time + object_display_time);
+            disp_response = setTimeout(function(){
+			    show_SOA();}, ITI_time + scene_display_time + object_display_time + gabor_display_time);
+		}
+	};
+
+    var finish = function() {
+		this.s = Snap('#svgMain');
+		probeinst1 = s.text(540,400,'You completed the experiment. Thank you! Press spacebar to continue.').attr({'fill' : 'white',  'stroke': 'white', 'stroke-width': 0.6});
+		document.addEventListener("keydown",to_questionnaire, false);
+	};
+
+    var to_questionnaire = function(e){
+        if (e.keyCode == 32) {
+            document.removeEventListener("keydown",to_questionnaire,false);
+            currentview = new Questionnaire();
         }
-        psiTurk.recordTrialData({"phase": "PRACTICE",
-            "trial": prac_trial_count,
+    };
+
+    var show_objects = function() {
+        this.s = Snap("#svgMain");
+
+        this.main_object_stim   = [exp_trials[trial_count][index_main_object]];
+        this.other_object_stim  = [exp_trials[trial_count][index_other_object]];
+        this.main_object_path   = "static/images/objects/" + this.main_object_stim + img_file_ext; // path to image
+        this.other_object_path  = "static/images/objects/" + this.other_object_stim + img_file_ext; // path to image
+
+        if (object_location[trial_count] < .5) {
+            this.main_object_location_x     = left_loc_object[0];
+            this.main_object_location_y     = left_loc_object[1];
+            this.other_object_location_x    = right_loc_object[0];
+            this.other_object_location_y    = right_loc_object[1];
+        } else {
+            this.main_object_location_x     = right_loc_object[0];
+            this.main_object_location_y     = right_loc_object[1];
+            this.other_object_location_x    = left_loc_object[0];
+            this.other_object_location_y    = left_loc_object[1];
+        }
+
+        this.s.image(this.main_object_path, this.main_object_location_x, this.main_object_location_y, object_size[0], object_size[1]);
+        this.s.image(this.other_object_path, this.other_object_location_x, this.other_object_location_y, object_size[0], object_size[1]);
+    };
+
+    var show_gabors = function() {
+        this.s = Snap("#svgMain");
+        var orient_match    = Math.random();
+        var match_determine = Math.random();
+
+        gabor_onset = new Date().getTime();
+        // Determine the orientation of target gabor
+        if (orient_match < .5) {
+            target_ori = -45;
+        } else {
+            target_ori = 45;
+        }
+
+        // Determine whether gabor orientations match
+        if (match_determine < .5) {
+            match = 1;
+            var center_ori = target_ori;
+        } else {
+            match = 0;
+            var center_ori = -target_ori;
+        }
+        // console.log(match);
+        if (parseInt([exp_trials[trial_count][index_target_loc]]) === 1) {
+            this.gabor_location     = left_loc_gabor;
+            this.distractor_location = right_loc_gabor;
+        } else {
+            this.gabor_location = right_loc_gabor;
+            this.distractor_location = left_loc_gabor;
+
+        }
+        this.target_gabor   = this.s.image("static/images/gabors/gabor01.png", this.gabor_location[0], this.gabor_location[1], target_gabor_size, target_gabor_size);
+        this.center_gabor   = this.s.image("static/images/gabors/gabor02.png", center_loc_gabor[0],center_loc_gabor[1], center_gabor_size, center_gabor_size);
+        this.distract_gabor = this.s.image("static/images/gabors/distractor.png", this.distractor_location[0],this.distractor_location[1], target_gabor_size, target_gabor_size);
+
+        // rotates gabor patch
+        this.target_gabor.transform("r"+parseInt(target_ori));
+        this.center_gabor.transform("r"+parseInt(center_ori));
+
+    };
+    // need to figure out how to exit this when no keypress
+    var show_SOA = function(){
+        this.s = Snap("#svgMain");
+        show_scene("static/images/scenes/" + [exp_trials[trial_count][index_scene_category]] +"/" +[exp_trials[trial_count][index_scene_exemplar]]+ img_file_ext);
+        show_objects();
+        show_fixation();
+
+        document.addEventListener("keypress", get_response, false);
+
+        prac_trial_count ++;
+        // console.log("end of trial:" + prac_trial_count);
+        // clearTimeout(show_gabors);
+	};
+
+    // need to restrict key press to f and j
+    var get_response = function (e) {
+        if (e.charCode === 102 || e.charCode === 106) { // 102 = f 106 = j
+
+            var RT = new Date().getTime() - gabor_onset; // Get RT
+            document.removeEventListener("keypress", get_response, false);
+            resp = 1;
+
+
+            if (e.charCode === 102 & match === 1) {
+                acc = 1;
+            } else if (e.charCode === 106 & match === 0) {
+                acc = 1;
+            } else {
+                acc = 0;
+            }
+            console.log("keyPressed: " + e.charCode, ", resp:" + resp + ", Acc: " + acc + ", RT: " + RT);
+
+            var ITI = function(){
+                this.s = Snap("#svgMain");
+                s.clear();
+                if (acc === 1) {
+                    show_fixation();
+                } else if (acc === 0) {
+                    show_fixation();
+                    this.fixation.attr({
+                        stroke: "#da2822"
+                    });
+                }
+                handle_fin = setTimeout(function(){
+                    rest()}, 500);
+            };
+            ITI();
+
+        } else {
+            resp    = 0;
+            acc     = 99;
+            handle_fin = setTimeout(function(){
+                disp_plz_respond()}, 100);
+
+
+        }
+        psiTurk.recordTrialData({"phase": "EXPERIMENT",
+            "trial": trial_count,
             "match" : match,
             "target_ori": target_ori,
-            "target_location": [prac_trials[prac_trial_count][index_target_loc]],
-            "scene_type": [prac_trials[prac_trial_count][index_scene_category]],
+            "target_location": [exp_trials[trial_count][index_target_loc]],
+            "scene_type": [exp_trials[trial_count][index_scene_category]],
             "RT": RT
             // "scene_exemplar": null,
         });
@@ -333,10 +521,10 @@ var practice = function() {
 
     // Adds rest inbetween blocks
     var rest = function() {
-        if (prac_trial_count % block_size == 0 && prac_trial_count != prac_length) {
+        if (trial_count % block_size == 0 && trial_count != prac_length) {
             this.s = Snap("#svgMain");
             s.clear();
-            var block_count = prac_trial_count/block_size;
+            var block_count = trial_count/block_size;
             var progress = total_blocks - block_count;
             var str1 = progress.toString();
             var str2 = " out of 4 blocks left. Please take a break. Press spacebar to continue";
