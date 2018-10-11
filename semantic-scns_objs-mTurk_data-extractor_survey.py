211152c7a7bdcdfbb67e9d9a07887666342fd485
Joseph Nah <joecool890@gmail.com>
2018-10-11 07:40:25 -0400

    # updates
    Signed-off-by: Joseph Nah <joecool890@gmail.com>

--- semantic-scns_objs-mTurk_data-extractor_survey.py
+++ semantic-scns_objs-mTurk_data-extractor_survey.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 # if you have workers you wish to exclude, add them here
 exclude     = ["A1V2H0UF94ATWY:36H9ULYP63SVHLP2L4S07K361DJFJV",
-               "APY5858P6BTDY:3H8DHMCCWA9TULHOCJXMKZ0Y4P6DKK"];
+               "APY5858P6BTDY:3H8DHMCCWA9TULHOCJXMKZ0Y4P6DKK"]
 
 # If on windows, change directory
 if  os.name == "nt":
@@ -27,18 +27,21 @@ data = pd.read_csv(data_files[0], index_col="uniqueId", header=0)
 
 
 raw_data.append(data)
-dataframe_raw = pd.concat(raw_data)
+df_raw = pd.concat(raw_data)
 
 # Select only uniqueID + survey data
-dataframe_raw = dataframe_raw.iloc[:,17:-1]
-dataframe_raw = dataframe_raw.iloc[2:]
+# as well as other cleaning methods
+df_raw = df_raw.iloc[:,17:-1]
+df_raw = df_raw.iloc[2:]
+df_raw = df_raw.drop(exclude)
+df_raw = df_raw.dropna()
 
 # remap string values into numbers for survey
 remapping = {"Extremely unlikely": 1, "Moderately unlikely": 2, "Slightly unlikely": 3, "Slightly likely": 4, "Moderately likely": 5, "Extremely likely": 6}
-dataframe = dataframe_raw.replace(remapping)
+df = df_raw.replace(remapping)
 
-data = dataframe.groupby(["uniqueId"]).mean()
-print(data)
+df["scene1"] = (df["Q1-10"] + df["Q1-11"])/2 - (df["Q1-20"] + df["Q1-21"] + df["Q1-30"] + df["Q1-31"] + df["Q1-40"] + df["Q1-41"] + df["Q1-50"] + df["Q1-51"])/8
+df["scene2"] = (df["Q2-20"] + df["Q2-21"])/2 - (df["Q2-10"] + df["Q2-11"] + df["Q2-30"] + df["Q2-31"] + df["Q2-40"] + df["Q2-41"] + df["Q2-50"] + df["Q2-51"])/8
 
-
-dataframe.to_clipboard(excel=True, sep='\t')
+data = df.groupby(["uniqueId"]).mean()
+print(df)
