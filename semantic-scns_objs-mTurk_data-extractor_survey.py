241efa521922f8297bb2eb3737f3e8d0396d82c8
Joseph Nah_lab <joecool890@gmail.com>
2018-10-11 16:00:49 -0400

    # Updated w/ Survey

--- semantic-scns_objs-mTurk_data-extractor_survey.py
+++ semantic-scns_objs-mTurk_data-extractor_survey.py
@@ -31,7 +31,7 @@ df_raw = pd.concat(raw_data)
 
 # Select only uniqueID + survey data
 # as well as other cleaning methods
-df_raw = df_raw.iloc[:,17:-1]
+df_raw = df_raw.iloc[:,17:]
 df_raw = df_raw.iloc[2:]
 df_raw = df_raw.drop(exclude)
 df_raw = df_raw.dropna()
@@ -40,8 +40,25 @@ df_raw = df_raw.dropna()
 remapping = {"Extremely unlikely": 1, "Moderately unlikely": 2, "Slightly unlikely": 3, "Slightly likely": 4, "Moderately likely": 5, "Extremely likely": 6}
 df = df_raw.replace(remapping)
 
-df["scene1"] = (df["Q1-10"] + df["Q1-11"])/2 - (df["Q1-20"] + df["Q1-21"] + df["Q1-30"] + df["Q1-31"] + df["Q1-40"] + df["Q1-41"] + df["Q1-50"] + df["Q1-51"])/8
-df["scene2"] = (df["Q2-20"] + df["Q2-21"])/2 - (df["Q2-10"] + df["Q2-11"] + df["Q2-30"] + df["Q2-31"] + df["Q2-40"] + df["Q2-41"] + df["Q2-50"] + df["Q2-51"])/8
+# Calculate average ratings for objects in appropriate scene
+df["scene1"] = (df["Q1-10"] + df["Q1-11"])/2
+df["scene2"] = (df["Q2-20"] + df["Q2-21"])/2
+df["scene3"] = (df["Q3-30"] + df["Q3-31"])/2
+df["scene4"] = (df["Q4-40"] + df["Q4-41"])/2
+df["scene5"] = (df["Q5-50"] + df["Q5-51"])/2
 
-data = df.groupby(["uniqueId"]).mean()
+# Calculate difference in ratings for objects in scene
+# Scene 1: Office
+# Scene 2: Living room
+# Scene 3: Bathroom
+# Scene 4: Kitchen
+# Scene 5: Bedroom
+df["scene1_sem-mag"] = (df["Q1-10"] + df["Q1-11"])/2 - (df["Q1-20"] + df["Q1-21"] + df["Q1-30"] + df["Q1-31"] + df["Q1-40"] + df["Q1-41"] + df["Q1-50"] + df["Q1-51"])/8
+df["scene2_sem-mag"] = (df["Q2-20"] + df["Q2-21"])/2 - (df["Q2-10"] + df["Q2-11"] + df["Q2-30"] + df["Q2-31"] + df["Q2-40"] + df["Q2-41"] + df["Q2-50"] + df["Q2-51"])/8
+df["scene3_sem-mag"] = (df["Q3-30"] + df["Q3-31"])/2 - (df["Q3-10"] + df["Q3-11"] + df["Q3-20"] + df["Q3-21"] + df["Q3-30"] + df["Q3-31"] + df["Q3-40"] + df["Q3-41"])/8
+df["scene4_sem-mag"] = (df["Q4-40"] + df["Q4-41"])/2 - (df["Q4-10"] + df["Q4-11"] + df["Q4-20"] + df["Q4-21"] + df["Q4-30"] + df["Q4-31"] + df["Q4-50"] + df["Q4-51"])/8
+df["scene5_sem-mag"] = (df["Q5-50"] + df["Q5-51"])/2 - (df["Q5-10"] + df["Q5-11"] + df["Q5-20"] + df["Q5-21"] + df["Q5-30"] + df["Q5-31"] + df["Q5-40"] + df["Q5-41"])/8
+
+# data = df.groupby(["uniqueId"]).mean()
 print(df)
+df.to_clipboard(excel=True, sep='\t')
