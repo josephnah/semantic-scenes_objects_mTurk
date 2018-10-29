ffa0b5396ae6318c0847ee0d0836ac1c05a9d55d
Joseph Nah <joecool890@gmail.com>
2018-10-29 01:18:48 -0400

    # Added survet data
    Signed-off-by: Joseph Nah <joecool890@gmail.com>

--- semantic-scns_objs-mTurk_data-organizer.py
+++ semantic-scns_objs-mTurk_data-organizer.py
@@ -6,6 +6,7 @@ import glob, os, time, getpass
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime as dt
 
 # Data Thresholding
 exp_name    = "scns_obs-mTurk"
@@ -13,6 +14,26 @@ RT_thres    = 1
 exp_iter    = 1
 scn_filter  = 0
 acc_thresh  = 80
+date        = dt.datetime.now().strftime("%Y-%m-%d")  # get date as YMD_H_M_S
+
+# function to determine confidence interval corrected for within-participants
+def cm_standard_error(dataframe):
+    # Get # of conditions (number of columns)
+    num_conditions = dataframe.shape[1]
+
+    # Morey portion of Cousineau-Morey method
+    morey_correction = np.sqrt(float(num_conditions) / (num_conditions - 1))
+
+    # Calculate mean of each participant and the grand mean
+    parMean     = dataframe.mean(axis = 1)
+    grandMean   = parMean.mean()
+
+    # Get standard error & add to original dataframe (subtract parMean, add grandMean)
+    normMean                        = dataframe.sub(parMean, axis=0) + grandMean
+    dataframe.loc["mean"]           = normMean.mean()
+    dataframe.loc["ste_c-m-corr"]   = normMean.std() / np.sqrt(len(dataframe) - 1) * morey_correction
+    forexcel_dataframe              = pd.concat([dataframe.loc["mean"], dataframe.loc["ste_c-m-corr"]], axis=1).unstack()
+    return(forexcel_dataframe)
 
 # Control which analysis to run
 data_analysis   = 1
@@ -27,10 +48,11 @@ if data_analysis == 1:
 
     # change directory based on computer
     userName    = getpass.getuser()
-    data_dir    = "/Users/" + userName + "/Dropbox/GWU/01_Research/08_semanticScenes/mTurk/data" + "/*.csv"
-    demogr_dir  = "/Users/" + userName + "/Dropbox/GWU/01_Research/08_semanticScenes/mTurk/demogr" + "/*.csv"
-    survey_dir  = "/Users/" + userName + "/Dropbox/GWU/01_Research/08_semanticScenes/mTurk/survey" + "/*.csv"
-    final_data  = "/Users/" + userName + "/Dropbox/GWU/01_Research/08_semanticScenes/mTurk/" + "semantic_scns-objs_mTurk.xlsx"
+    data_dir    = "/Users/" + userName + "/Dropbox/GWU/01_Research/08_semanticScenes/data/exp04-mTurk/data" + "/*.csv"
+    demogr_dir  = "/Users/" + userName + "/Dropbox/GWU/01_Research/08_semanticScenes/data/exp04-mTurk/demogr" + "/*.csv"
+    survey_dir  = "/Users/" + userName + "/Dropbox/GWU/01_Research/08_semanticScenes/data/exp04-mTurk/survey" + "/*.csv"
+    final_data  = "/Users/" + userName + "/Dropbox/GWU/01_Research/08_semanticScenes/data/exp04-mTurk/" + "semantic_scns-objs_mTurk_" + date + ".xlsx"
+
     # Extract all data files into single np array
     data_files      = glob.glob(data_dir)
     survey_files    = glob.glob(demogr_dir)
@@ -39,6 +61,7 @@ if data_analysis == 1:
     # Load all data and survey files into single panda data frame
     raw_data    = []
     demogr_data = []
+    x = time.time()
     for file in range(participants):
         data    = pd.read_csv(data_files[file], header = 0)
         survey  = pd.read_csv(survey_files[file], header = 0)
@@ -99,46 +122,35 @@ if data_analysis == 1:
         excluded_data_count = pd.concat(excluded_data_count, axis=1)
         excluded_data_count.columns = ["dropped_trials", "drop_rate"]
 
-    # COLUMNS
-    col_semRel_scenes           = ["SR_office", "SR_living", "SR_bath", "SR_kitchen", "SR_bed", "NR_office", "NR_living",
-                                   "NR_bath", "NR_kitchen", "NR_bed"]
-
-    col_semRel_scenes_mainObj   = ["SR_office_mouse","SR_office_calc", "SR_living_remote", "SR_living_game", "SR_bath_paste",
-                                   "SR_bath_tp", "SR_kitchen_mitt", "SR_kitchen_grater", "SR_bed_hanger", "SR_bed_clock",
-                                   "NR_office_mouse", "NR_office_calc", "NR_living_remote", "NR_living_game", "NR_bath_paste",
-                                   "NR_bath_tp", "NR_kitchen_mitt", "NR_kitchen_grater", "NR_bed_hanger", "NR_bed_clock"]
-
     # Keep only the good participants
-    acc_overall                 = exp_dataframe.groupby(["uniqueid"])["accuracy"].mean()*100
-    good_participants           = acc_overall.loc[acc_overall >= acc_thresh].index.get_level_values(0).tolist()
+    acc_cut                     = exp_dataframe.groupby(["uniqueid"])["accuracy"].mean()*100
+    good_participants           = acc_cut.loc[acc_cut >= acc_thresh].index.get_level_values(0).tolist()
     exp_dataframe               = exp_dataframe[exp_dataframe["uniqueid"].isin(good_participants)]
+    dropped_trials              = excluded_data_count[excluded_data_count.index.isin(good_participants)]
+
+    # Remap hand to numbers
+    exp_dataframe["hand"]       = exp_dataframe["hand"].map({"Right": 1, "Left": 2})
+    demogr_dataframe            = exp_dataframe.groupby(["uniqueid"])["age", "hand"].mean()
 
     # ACCURACY
+    acc_overall                 = exp_dataframe.groupby(["uniqueid"])["accuracy"].mean() * 100
     acc_semRel                  = exp_dataframe.groupby(["uniqueid","condition"])["accuracy"].mean().unstack(["condition"])*100
-    acc_semRel_scenes           = exp_dataframe.groupby(["uniqueid","scene_type","condition"])["accuracy"].mean().unstack(["condition","scene_type"])*100
-    acc_semRel_scenes_mainObj   = exp_dataframe.groupby(["uniqueid","main_obj"])["accuracy"].mean().unstack(["main_obj"])*100
-
-    # acc_semRel_scenes_mainObj.columns = col_semRel_scenes_mainObj;
+    acc_semRel_scenes           = exp_dataframe.groupby(["uniqueid","scene_type","condition"])["accuracy"].mean().unstack(["scene_type", "condition"])*100
+    acc_semRel_scenes_exemplar  = exp_dataframe.groupby(["uniqueid", "scene_type", "scene_exemplar", "condition"])["accuracy"].mean().unstack(["condition", "scene_type", "scene_exemplar"])*100
+    acc_semRel_scenes_mainObj   = exp_dataframe.groupby(["uniqueid","scene_type","main_obj", "condition"])["accuracy"].mean().unstack(["scene_type","main_obj", "condition"])*100
 
     # RT
+    # Get only correct data
     corr_exp_dataframe          = exp_dataframe[(exp_dataframe["accuracy"] == 1)]
 
     RT_overall                  = corr_exp_dataframe.groupby(["uniqueid"])["RT"].mean()
     RT_semRel                   = corr_exp_dataframe.groupby(["uniqueid","condition"])["RT"].mean().unstack(["condition"])
-    RT_semRel_scenes            = corr_exp_dataframe.groupby(["uniqueid","scene_type","condition"])["RT"].mean().unstack(["condition","scene_type"])
+    RT_semRel_scenes            = corr_exp_dataframe.groupby(["uniqueid","scene_type","condition"])["RT"].mean().unstack(["scene_type", "condition"])
     RT_semRel_scenes_exemplar   = corr_exp_dataframe.groupby(["uniqueid","scene_type","scene_exemplar","condition"])["RT"].mean().unstack(["condition","scene_type","scene_exemplar"])
-    RT_semRel_scenes_mainObj    = corr_exp_dataframe.groupby(["uniqueid","main_obj"])["RT"].mean().unstack(["main_obj"])
+    RT_semRel_scenes_mainObj    = corr_exp_dataframe.groupby(["uniqueid","scene_type","main_obj", "condition"])["RT"].mean().unstack(["scene_type","main_obj", "condition"])
 
-    # RT_semRel_scenes_mainObj.columns = col_semRel_scenes_mainObj;
     # concat all data into single data frame, update when necessary
     all_data = pd.concat([RT_semRel_scenes_mainObj],axis = 1)
-
-    # print(all_data.describe())
-    a = all_data.mean(axis = 0)
-
-
-    # print(RT_semRel_scenes_mainObj)
-    RT_semRel_scenes.to_clipboard(excel=True, sep='\t')
 
     # PLOT GRAPHS
     if plot_graphs == 1:
@@ -164,15 +176,14 @@ if survey_analysis == 1:
     raw_data            = pd.read_csv(survey_files[0], header=0)
     survey_data.append(raw_data)
     survey_dataframe    = pd.concat(survey_data)
+    bad_participants    = []
 
     # only select data from good participants
     survey_dataframe    = survey_dataframe[survey_dataframe["uniqueId"].isin(good_participants)]
-    print(len(good_participants))
-    print(survey_dataframe)
-    # Select only uniqueID + survey data
+
     # Drops irrelevant columns (date, etc)
     survey_dataframe = survey_dataframe.iloc[:,17:]
-    survey_dataframe = survey_dataframe.iloc[2:]
+
     # remap string values into numbers for survey
     remapping = {"Extremely unlikely": 1, "Moderately unlikely": 2, "Slightly unlikely": 3, "Slightly likely": 4, "Moderately likely": 5, "Extremely likely": 6,
                  "Very Non-Representative": 1, "Moderately Non-Representative": 2, "Slightly Non-Representative": 3, "Slightly Representative": 4, "Moderately Representative": 5, "Very Representative": 6}
@@ -200,6 +211,37 @@ if survey_analysis == 1:
     survey_dataframe["scene4_sem-mag"] = (survey_dataframe["Q4-40"] + survey_dataframe["Q4-41"])/2 - (survey_dataframe["Q4-10"] + survey_dataframe["Q4-11"] + survey_dataframe["Q4-20"] + survey_dataframe["Q4-21"] + survey_dataframe["Q4-30"] + survey_dataframe["Q4-31"] + survey_dataframe["Q4-50"] + survey_dataframe["Q4-51"])/8
     survey_dataframe["scene5_sem-mag"] = (survey_dataframe["Q5-50"] + survey_dataframe["Q5-51"])/2 - (survey_dataframe["Q5-10"] + survey_dataframe["Q5-11"] + survey_dataframe["Q5-20"] + survey_dataframe["Q5-21"] + survey_dataframe["Q5-30"] + survey_dataframe["Q5-31"] + survey_dataframe["Q5-40"] + survey_dataframe["Q5-41"])/8
 
-    # print(survey_dataframe.describe())
-    # survey_dataframe.describe().to_clipboard(excel=True, sep='\t')
-    # Get scene representative data
+    # Differences for each object
+    survey_dataframe["10_sem-mag"] = survey_dataframe["Q1-10"] - (survey_dataframe["Q1-20"] + survey_dataframe["Q1-21"] + survey_dataframe["Q1-30"] + survey_dataframe["Q1-31"] + survey_dataframe["Q1-40"] + survey_dataframe["Q1-41"] + survey_dataframe["Q1-50"] + survey_dataframe["Q1-51"])/8
+    survey_dataframe["11_sem-mag"] = survey_dataframe["Q1-11"] - (survey_dataframe["Q1-20"] + survey_dataframe["Q1-21"] + survey_dataframe["Q1-30"] + survey_dataframe["Q1-31"] + survey_dataframe["Q1-40"] + survey_dataframe["Q1-41"] + survey_dataframe["Q1-50"] + survey_dataframe["Q1-51"])/8
+    survey_dataframe["20_sem-mag"] = survey_dataframe["Q2-20"] - (survey_dataframe["Q2-10"] + survey_dataframe["Q2-11"] + survey_dataframe["Q2-30"] + survey_dataframe["Q2-31"] + survey_dataframe["Q2-40"] + survey_dataframe["Q2-41"] + survey_dataframe["Q2-50"] + survey_dataframe["Q2-51"])/8
+    survey_dataframe["21_sem-mag"] = survey_dataframe["Q2-21"] - (survey_dataframe["Q2-10"] + survey_dataframe["Q2-11"] + survey_dataframe["Q2-30"] + survey_dataframe["Q2-31"] + survey_dataframe["Q2-40"] + survey_dataframe["Q2-41"] + survey_dataframe["Q2-50"] + survey_dataframe["Q2-51"])/8
+    survey_dataframe["30_sem-mag"] = survey_dataframe["Q3-30"] - (survey_dataframe["Q3-10"] + survey_dataframe["Q3-11"] + survey_dataframe["Q3-20"] + survey_dataframe["Q3-21"] + survey_dataframe["Q3-30"] + survey_dataframe["Q3-31"] + survey_dataframe["Q3-40"] + survey_dataframe["Q3-41"])/8
+    survey_dataframe["31_sem-mag"] = survey_dataframe["Q3-31"] - (survey_dataframe["Q3-10"] + survey_dataframe["Q3-11"] + survey_dataframe["Q3-20"] + survey_dataframe["Q3-21"] + survey_dataframe["Q3-30"] + survey_dataframe["Q3-31"] + survey_dataframe["Q3-40"] + survey_dataframe["Q3-41"])/8
+    survey_dataframe["40_sem-mag"] = survey_dataframe["Q4-40"] - (survey_dataframe["Q4-10"] + survey_dataframe["Q4-11"] + survey_dataframe["Q4-20"] + survey_dataframe["Q4-21"] + survey_dataframe["Q4-30"] + survey_dataframe["Q4-31"] + survey_dataframe["Q4-50"] + survey_dataframe["Q4-51"])/8
+    survey_dataframe["41_sem-mag"] = survey_dataframe["Q4-41"] - (survey_dataframe["Q4-10"] + survey_dataframe["Q4-11"] + survey_dataframe["Q4-20"] + survey_dataframe["Q4-21"] + survey_dataframe["Q4-30"] + survey_dataframe["Q4-31"] + survey_dataframe["Q4-50"] + survey_dataframe["Q4-51"])/8
+    survey_dataframe["50_sem-mag"] = survey_dataframe["Q5-50"] - (survey_dataframe["Q5-10"] + survey_dataframe["Q5-11"] + survey_dataframe["Q5-20"] + survey_dataframe["Q5-21"] + survey_dataframe["Q5-30"] + survey_dataframe["Q5-31"] + survey_dataframe["Q5-40"] + survey_dataframe["Q5-41"])/8
+    survey_dataframe["51_sem-mag"] = survey_dataframe["Q5-51"] - (survey_dataframe["Q5-10"] + survey_dataframe["Q5-11"] + survey_dataframe["Q5-20"] + survey_dataframe["Q5-21"] + survey_dataframe["Q5-30"] + survey_dataframe["Q5-31"] + survey_dataframe["Q5-40"] + survey_dataframe["Q5-41"])/8
+
+    # Save Semantic difference score as separate dataframe
+    survey_semDiff_dataframe = survey_dataframe.iloc[:,95:]
+
+## Let's wrap up now
+# concat all data into single data frame, update when necessary
+all_data    = pd.concat([demogr_dataframe, RT_semRel, acc_semRel_scenes, RT_semRel_scenes, survey_semDiff_dataframe], axis=1)
+
+
+## Get data for graphing
+graph_acc   = cm_standard_error(acc_semRel_scenes_mainObj)
+graph_RT    = cm_standard_error(RT_semRel_scenes_mainObj)
+graph_surv  = survey_semDiff_dataframe.mean()
+print(survey_semDiff_dataframe)
+graph_data  = pd.concat([graph_acc, graph_RT], axis = 1)
+
+# Prepare data for regression here
+
+# copy df to clipboard
+survey_semDiff_dataframe.to_clipboard(excel=True, sep='\t')
+
+time_elapsed = time.time() - x
+print("Success! Time Elapsed: " + str(round(time_elapsed,5)*1000) + " ms")
